4ce74da65e6b79415f42c98a96eda9b10f5cda73
Felix Maina <maina.felix@eclectics.io>
2025-09-04 19:31:45 +0300

    add lecturer dashboard ts

---
+++ src/app/lecturer-dashboard/lecturer-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lecturer-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './lecturer-dashboard.component.html',
+  styleUrl: './lecturer-dashboard.component.css'
+})
+export class LecturerDashboardComponent {
+
+}
