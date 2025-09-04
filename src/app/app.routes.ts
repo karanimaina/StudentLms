b250bf4ac61d86b414778733c307125ca80baca6
Felix Maina <maina.felix@eclectics.io>
2025-09-04 19:30:08 +0300

    add app routes path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {RegisterComponent} from "./register/register.component";
+import {LoginComponent} from "./login/login.component";
+import {StudentDashboardComponent} from "./student-dashboard/student-dashboard.component";
+import {LecturerDashboardComponent} from "./lecturer-dashboard/lecturer-dashboard.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'signUp',component:RegisterComponent},
+  {path:'signIn',component:LoginComponent},
+  {path:'student',component:StudentDashboardComponent},
+  {path:'lecturer',component:LecturerDashboardComponent},
+  {path:'**',component:NotFoundComponent}
+];
