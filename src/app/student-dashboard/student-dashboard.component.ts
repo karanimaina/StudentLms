cd3143ce707989ba19ea7e865ff6715bb88ded40
Felix Maina <maina.felix@eclectics.io>
2025-09-05 14:49:29 +0300

    update  student dashboard

--- src/app/student-dashboard/student-dashboard.component.ts
+++ src/app/student-dashboard/student-dashboard.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {Router, RouterLink, RouterLinkActive} from "@angular/router";
+import {NavbarComponent} from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-student-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    NavbarComponent
+  ],
   templateUrl: './student-dashboard.component.html',
   styleUrl: './student-dashboard.component.css'
 })
 export class StudentDashboardComponent {
+
+  constructor(route:Router) {
+  }
 
 }
