b50022e061a8ee30ac9b7a9498e1f84514430551
Felix Maina <maina.felix@eclectics.io>
2025-09-05 23:36:14 +0300

    add session component

--- src/app/session/session.component.ts
+++ src/app/session/session.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {NavbarComponent} from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-session',
   standalone: true,
-  imports: [],
+  imports: [
+    NavbarComponent
+  ],
   templateUrl: './session.component.html',
   styleUrl: './session.component.css'
 })
