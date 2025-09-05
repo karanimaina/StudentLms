6e1f6daa9167a630ec31e039c680efcd495f4dd2
Felix Maina <maina.felix@eclectics.io>
2025-09-05 14:48:10 +0300

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,15 @@ import {LoginComponent} from "./login/login.component";
 import {StudentDashboardComponent} from "./student-dashboard/student-dashboard.component";
 import {LecturerDashboardComponent} from "./lecturer-dashboard/lecturer-dashboard.component";
 import {NotFoundComponent} from "./not-found/not-found.component";
+import {CourseComponent} from "./course/course.component";
+import {SessionComponent} from "./session/session.component";
 
 export const routes: Routes = [
   {path:'signUp',component:RegisterComponent},
   {path:'signIn',component:LoginComponent},
   {path:'student',component:StudentDashboardComponent},
   {path:'lecturer',component:LecturerDashboardComponent},
+  {path:'course',component:CourseComponent},
+  {path:'session',component:SessionComponent},
   {path:'**',component:NotFoundComponent}
 ];
